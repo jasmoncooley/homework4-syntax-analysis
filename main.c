41c42602960737574bee51927fa133bc6054f573
Ikechukwu <ikechukwuanude@gmail.com>
2020-03-10 23:58:32 +0000

    added the front.in file - spaced it out correctly

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// THIS IS ALL TEXTBOOK CODE
+  // THIS IS ALL TEXTBOOK CODE
 
 
 /* front.c - a lexical analyzer system for simple
@@ -39,70 +39,70 @@ int lex();
 /* main driver */
 main() {
 /* Open the input data file and process its contents */
- if ((in_fp = fopen("front.in", "r")) == NULL)
- printf("ERROR - cannot open front.in \n");
- else {
- getChar();
- do {
- lex();
- } while (nextToken != EOF);
- }
+  if ((in_fp = fopen("front.in", "r")) == NULL)
+  printf("ERROR - cannot open front.in \n");
+  else {
+    getChar();
+    do {
+      lex();
+    } while (nextToken != EOF);
+  }
 }
 
 /*****************************************************/
 /* lookup - a function to lookup operators and parentheses
  and return the token */
 int lookup(char ch) {
- switch (ch) {
- case '(':
- addChar();
- nextToken = LEFT_PAREN;
- break;
- case ')':
- addChar();
- nextToken = RIGHT_PAREN;
- break;
- case '+':
- addChar();
- nextToken = ADD_OP;
- break;
- case '-':
- addChar();
- nextToken = SUB_OP;
- break;
- case '*':
- addChar();
- nextToken = MULT_OP;
- break;
- case '/':
- addChar();
- nextToken = DIV_OP;
- break;
+  switch (ch) {
+    case '(':
+      addChar();
+      nextToken = LEFT_PAREN;
+      break;
+    case ')':
+      addChar();
+      nextToken = RIGHT_PAREN;
+      break;
+    case '+':
+      addChar();
+      nextToken = ADD_OP;
+      break;
+    case '-':
+      addChar();
+      nextToken = SUB_OP;
+      break;
+    case '*':
+      addChar();
+      nextToken = MULT_OP;
+      break;
+    case '/':
+      addChar();
+      nextToken = DIV_OP;
+      break;
 
-default:
- addChar();
- nextToken = EOF;
- break;
- }
- return nextToken;
+    default:
+      addChar();
+      nextToken = EOF;
+      break;
+  }
+  return nextToken;
 }
 /*****************************************************/
 /* addChar - a function to add nextChar to lexeme */
 void addChar() {
- if (lexLen <= 98) {
- lexeme[lexLen++] = nextChar;
- lexeme[lexLen] = 0;
- }
- else
- printf("Error - lexeme is too long \n");
+  if (lexLen <= 98) {
+    lexeme[lexLen++] = nextChar;
+    lexeme[lexLen] = 0;
+  }
+  else
+    printf("Error - lexeme is too long \n");
 }
 /*****************************************************/
 /* getChar - a function to get the next character of
  input and determine its character class */
 void getChar() {
- if ((nextChar = getc(in_fp)) != EOF) {
- if (isalpha(nextChar))
- charClass = LETTER;
+  if ((nextChar = getc(in_fp)) != EOF){
+    if (isalpha(nextChar))
+    charClass = LETTER;
  else if (isdigit(nextChar))
  charClass = DIGIT;
  else charClass = UNKNOWN;
@@ -117,8 +117,7 @@ void getNonBlank() {
  while (isspace(nextChar))
  getChar();
 }
-/
-*****************************************************/
+/*****************************************************/
 /* lex - a simple lexical analyzer for arithmetic
  expressions */
 int lex() {
